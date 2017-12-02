e60e9956c948851fdd2c6be9319b87cb33627e34
Yu Hsun Lee <andylee0914.tw@gmail.com>
2017-12-02 11:33:42 +0800

    update
    update

---
+++ school_place.py
@@ -0,0 +1,46 @@
+import requests
+import csv
+foldername = 'reg_data'
+api_key = ''
+data = []
+error = []
+with open(foldername + '/schools.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        try:
+            print(row["name"])
+            payload = {'input': row["name"], 'types': 'geocode',
+                       'language': 'zh_TW', 'key': api_key}
+            r = requests.get(
+                "https://maps.googleapis.com/maps/api/place/queryautocomplete/json", params=payload)
+            res = r.json()
+            # 
+            if res['status'] == "ZERO_RESULTS":
+                payload = {'input': row["name"].replace('高中','中學'), 'types': 'geocode',
+                           'language': 'zh_TW', 'key': api_key}
+                r = requests.get(
+                    "https://maps.googleapis.com/maps/api/place/queryautocomplete/json", params=payload)
+                res = r.json()
+            pid = res['predictions'][0]['place_id']
+            
+            payload1 = {'placeid': pid, 'key': api_key, 'language': 'zh_TW'}
+            r1 = requests.get(
+                "https://maps.googleapis.com/maps/api/place/details/json", params=payload1)
+            res1 = r1.json()
+            res1 = res1['result']
+            temp = [row["name"],res1['name'], res1['formatted_address'],
+                    res1['geometry']['location']['lat'], res1['geometry']['location']['lng']]
+            data.append(temp)
+        except :
+            error.append(row["name"])
+        
+print(error)
+with open(foldername + '/school_place.csv', 'w', newline='') as csvfile:
+        fieldnames = ['school','name','formatted_address', 'lat', 'lng']
+        writer1 = csv.writer(csvfile)
+        writer1.writerow(fieldnames)
+        for dg in data:
+            print(dg)
+            writer1.writerow(dg)
+
+        
--- textrank.py
+++ textrank.py
@@ -10,15 +10,18 @@ file = sorted(os.listdir(full_folder))
 jieba.set_dictionary(foldername + '/dict.txt.big.txt')
 if not os.path.exists(foldername + '/textrank/'):
         os.makedirs(foldername + '/textrank/')
-
+# file = [ 'men.txt','women.txt']
 for fn in file:
     try:
         print(fn)
         csvname = fn.replace('txt', 'csv')
         with open(foldername + '/purpose/' + fn, 'r') as file:
-            data = file.read().replace('研究經驗及心得', '').replace('報名目的', '').replace(
+            # f = open(foldername + '/' + fn, 'w')
+            data = file.read().replace('研究經驗及心得', '').replace('報名目的', '').replace('報名⽬的', '').replace(
                 '\n', '').replace('若組員有撰寫小論文或是參加科展等活動的經驗，請簡述活動經驗及其心得（無則免）。','')
-            seg_list = jieba.analyse.textrank(data, topK=30, withWeight=True)
+            # f.write(data)
+            # f.close()
+            seg_list = jieba.analyse.textrank(data, topK=200, withWeight=True)
             with open(foldername + '/textrank/' + csvname, 'w', newline='') as csvfile:
                 csv_out = csv.writer(csvfile)
                 csv_out.writerow(['keyword', 'textrank'])
